a067e99b1c9f3d54ecde49f14d0c654266b33c7b
alisadermen <alisadermen@gmail.com>
2020-11-01 18:24:14 +0000

    ubung 5

--- src/exercise5/AscendingSort.java
+++ src/exercise5/AscendingSort.java
@@ -3,6 +3,8 @@ package exercise5;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.NumberOfInterveningJobs;
+
 public class AscendingSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -17,7 +19,16 @@ public class AscendingSort {
             numbers[index] = input.nextInt();
         }
 
-        // Sort numbers in ascending order
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                int next = j + 1;
+                if (numbers[j] > numbers[next]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[next];
+                    numbers[next] = temp;
+                }
+            }
+        }
 
         String numbersAsString = Arrays.toString(numbers);
         System.out.println("Sorted numbers: " + numbersAsString);
